3fd8d91663ec30397dcf7c7932862676de9ed5d1
vincentsmid <vincent.smid@seznam.cz>
2024-03-25 16:37:38 +0100

    🧪 Slic3r test

--- main_app/api/api_v1/endpoints/calculatePrice.py
+++ main_app/api/api_v1/endpoints/calculatePrice.py
@@ -1,5 +1,7 @@
 import logging
 
+import subprocess
+
 from dotenv import load_dotenv
 
 from fastapi import APIRouter, Form, Request, File, UploadFile
@@ -28,5 +30,16 @@ async def calculate_price(to_calculate: UploadFile = File(...)):
   file_extension = os.path.splitext(to_calculate.filename)[1]
   if file_extension.lower() != ".stl":
     return JSONResponse(status_code=400, content={"message": "Invalid file format. Only .stl files are allowed."})
+  
+  command = "slic3r --help"
+
+  # Execute the command
+  result = subprocess.run(command, shell=True, capture_output=True, text=True)
+
+  # Print the output
+  print(result.stdout)
+
+  if result.stderr:
+    print("Error:", result.stderr)
   
   return {"price": 0.0}
