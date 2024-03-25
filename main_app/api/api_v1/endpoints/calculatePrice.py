df36e4adc27a5505b91d8833e4f57e5e10f18fcb
vincentsmid <vincent.smid@seznam.cz>
2024-03-25 14:54:25 +0100

    ✨ calculatePrices endpoint

---
+++ main_app/api/api_v1/endpoints/calculatePrice.py
@@ -0,0 +1,32 @@
+import logging
+
+from dotenv import load_dotenv
+
+from fastapi import APIRouter, Form, Request, File, UploadFile
+import os
+from fastapi.responses import JSONResponse
+
+router = APIRouter()
+
+load_dotenv()
+
+
+
+router = APIRouter()
+
+@router.post("/")
+async def calculate_price(to_calculate: UploadFile = File(...)):
+  """
+  Calculate the price of the plastic based on the given data.
+
+  Parameters:
+  - to_calculate (file): The file containing the data to calculate the price.
+
+  Returns:
+  - JSONResponse: The response containing the calculated price.
+  """
+  file_extension = os.path.splitext(to_calculate.filename)[1]
+  if file_extension.lower() != ".stl":
+    return JSONResponse(status_code=400, content={"message": "Invalid file format. Only .stl files are allowed."})
+  
+  return {"price": 0.0}
--- main_app/api/api_v1/router.py
+++ main_app/api/api_v1/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
-# from main_app.api.api_v1.endpoints import ()
+from main_app.api.api_v1.endpoints import (calculatePrice)
 
 router = APIRouter()
+
+router.include_router(calculatePrice.router, prefix="/calculateprice", tags=["calculatePrice"])
