8fc655c235a42e9c1f226356ae5f8571242fe485
vincentsmid <vincent.smid@seznam.cz>
2024-03-31 00:55:55 +0100

    🗃️ Added migrations new to dev script

--- bin/dev/migrate.sh
+++ bin/dev/migrate.sh
@@ -1,5 +1,6 @@
 echo "Running migrations and creating first user..."
 
+poetry run piccolo migrations new main_app --auto
 poetry run piccolo migrations forwards all
 poetry run piccolo user create --username=admin --password=adminadminadmin --email=admin@gpcz.eu  --is_admin=t --is_superuser=t --is_active=t
 
