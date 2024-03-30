b6c148d25b976ad666006fe40aa38cb3535d2852
vincentsmid <vincent.smid@seznam.cz>
2024-03-30 23:10:28 +0100

    🗃️ Database model init

--- main_app/db_models.py
+++ main_app/db_models.py
@@ -1,13 +1,30 @@
-from piccolo.columns import Varchar, Integer, Timestamp, Real, Boolean
+from piccolo.columns import Varchar, Integer, Timestamp, Real, Boolean, ForeignKey
 from piccolo.table import Table
 
 
 class PotentialOrdersFromEstimate(Table):
-    orderID = Integer(primary_key=True)
-    orderValue = Varchar()
-    printTime = Real()
-    orderDate = Timestamp()
-    filamentUsed = Real()
-    filamentType = Varchar()
-    discordName = Varchar()
-    contacted = Boolean()
+  orderID = Integer(primary_key=True)
+  orderValue = Varchar()
+  printTime = Real()
+  orderDate = Timestamp()
+  filamentUsed = Real()
+  filamentType = Varchar()
+  discordName = Varchar()
+  contacted = Boolean()
+
+class FilamentsStock(Table):
+  filamentID = Integer(primary_key=True)
+  filamentType = ForeignKey(references=FilamentMaterials)
+  filamentStock = Integer()
+  filamentInfinite = Boolean()
+  filamentPrice = Real()
+  filamentName = Varchar()
+  filamentDescription = Varchar()
+
+class FilamentMaterials(Table):
+  filamentMaterial = Varchar(primary_key=True)
+  printTemperature = Integer()
+  bedTemperature = Integer()
+  difficultyIndex = Real()
+  timeIndex = Real()
+  multiplier = Real()
