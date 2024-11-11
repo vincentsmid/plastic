76267a725fe42d2b285ae9403ba42908854d952b
Vincent Smid <vincent.smid@seznam.cz>
2024-11-11 16:46:36 +0100

    🔨 Fix deploy scripts

--- bin/dev/migrate.sh
+++ bin/dev/migrate.sh
@@ -2,6 +2,6 @@ echo "Running migrations and creating first user..."
 
 poetry run piccolo migrations new main_app --auto
 poetry run piccolo migrations forwards all
-poetry run piccolo user create --username=admin --password=adminadminadmin --is_admin=t --is_superuser=t --is_active=t
+poetry run piccolo user create --username=admin --password=adminadminadmin --email=admin@plasticlab.xyz --is_admin=t --is_superuser=t --is_active=t
 
 echo "Migrations done"
--- bin/prod/migrate.sh
+++ bin/prod/migrate.sh
@@ -12,7 +12,7 @@ piccolo migrations forwards all
 check_error "Running migrations"
 
 echo "Creating admin user..."
-piccolo user create --username=admin --password= --is_admin=t --is_superuser=t --is_active=t
+piccolo user create --username=admin --password= --email=admin@plasticlab.xyz --is_admin=t --is_superuser=t --is_active=t
 check_error "Creating admin user"
 
 echo "Migrations and user creation completed successfully"
